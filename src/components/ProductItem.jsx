010912bcdb39055a0e6b2b76b8e08321b4814557
Gabriel Bracamonte <gabobrako@gmail.com>
2021-10-30 13:57:29 -0400

    uploading my project to Git

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,28 @@
+import React, {useContext} from 'react';
+import '@styles/ProductItem.scss';
+import btnAddToCart from '@icons/bt_add_to_cart.svg';
+import AppContext from '../context/AppContext';
+
+const ProductItem = ({product}) => {
+	const {addToCart } = useContext(AppContext);
+
+	const handleClick = item => {
+		addToCart (item);
+	}
+	return (
+		<div className="ProductItem">
+			<img src={product.image} alt={product.title} />
+			<div className="product-info">
+				<div>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
+				</div>
+				<figure onClick={() => handleClick(product)}>
+					<img src={btnAddToCart} alt="" />
+				</figure>
+			</div>
+		</div>
+	);
+}
+
+export default ProductItem;
---
+++ src/routes/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Layout from "../containers/Layout";
+import Login from "../pages/Login";
+import PasswordRecovery from "../pages/PasswordRecovery";
+import '../styles/global.css';
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
+import Checkout from '../pages/Checkout';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Orders from '../pages/Orders';
+import AppContext from "../context/AppContext";
+import useInitialState from "../hooks/useInitialState";
+
+const App = () => {
+    const initialState = useInitialState();
+    return(
+        <AppContext.Provider value={initialState}>
+            <BrowserRouter>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/recovery-password" component={PasswordRecovery} />
+                        <Route exact path="/checkout" component={Checkout} />
+                        <Route exact path="/create-account" component={CreateAccount} />
+                        <Route exact path="/my-account" component={MyAccount} />
+                        <Route exact path="/new-password" component={NewPassword} />
+                        <Route exact path="/orders" component={Orders} />
+                        <Route exact path="/send-email" component={SendEmail} />
+                        <Route path="*" component={NotFound} />
+                    </Switch>
+                </Layout>
+            </BrowserRouter>
+        </AppContext.Provider>
+        
+    );
+};
+
+export default App;
